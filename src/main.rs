35b4bb08e0dd62768b7190ccde5817629ddb6ba3
Erik Vesteraas <erik@vestera.as>
2019-02-08 14:03:49 +0100

    Make more things options

--- src/main.rs
+++ src/main.rs
@@ -19,9 +19,13 @@ pub struct Options {
     /// Output path
     pub output: String,
 
-    #[structopt(long, short, default_value = "50")]
-    /// Number of samples horizontally
-    pub samples: u32,
+    #[structopt(long, default_value = "300")]
+    /// Output width in mm
+    pub output_width: f64,
+
+    #[structopt(long, short, default_value = "5")]
+    /// Horizontal spacing between samples in mm
+    pub spacing: f64,
 
     #[structopt(long, default_value = "circle")]
     /// Shape used for samples. "circle" or "diamond"
@@ -36,8 +40,19 @@ fn main() -> Result<(), Box<dyn Error>> {
     let image_width = img.width() as f64;
     let image_height = img.height() as f64;
 
-    let samples_width = options.samples;
-    let samples_height = options.samples;
+    let image_ratio = image_width / image_height;
+
+    let spacing = options.spacing;
+    let output_width = options.output_width;
+    let output_height = output_width * image_ratio;
+
+    let spacing_ratio = 1.0;
+    let spacing_x = spacing;
+    let spacing_y = spacing_x * spacing_ratio;
+
+    let samples_width = (output_width / spacing) as u32;
+    let samples_height = (samples_width as f64 * image_ratio * spacing_ratio) as u32;
+
     let samples_width_f = samples_width as f64;
     let samples_height_f = samples_height as f64;
 
@@ -48,15 +63,18 @@ fn main() -> Result<(), Box<dyn Error>> {
             let pixel_x = (x as f64 / samples_width_f) * image_width;
             let pixel_y = (y as f64 / samples_height_f) * image_height;
             let pixel: Luma<u8> = img.get_pixel(pixel_x as u32, pixel_y as u32).to_luma();
-            let radius = (pixel.data[0] as f64 / 255.0) * 0.45;
+            let radius = (pixel.data[0] as f64 / 255.0) * spacing_x * 0.45;
 
             if radius < 0.08 {
                 continue;
             }
 
+            let sample_x = x as f64 * spacing_x;
+            let sample_y = y as f64 * spacing_y;
+
             let sample = match &*options.shape {
-                "diamond" => diamond(x.into(), y.into(), radius),
-                "circle" | _ => circle(x.into(), y.into(), radius),
+                "diamond" => diamond(sample_x.into(), sample_y.into(), radius),
+                "circle" | _ => circle(sample_x.into(), sample_y.into(), radius),
             };
             samples.push(sample);
         }
@@ -64,11 +82,11 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     let data = svg(
         vec![
-            ("width", "300mm".into()),
-            ("height", "300mm".into()),
+            ("width", format!("{}mm", output_width)),
+            ("height", format!("{}mm", output_height)),
             (
                 "viewBox",
-                format!("0 0 {} {}", samples_width, samples_height),
+                format!("0 0 {} {}", output_width, output_height),
             ),
             ("xmlns", "http://www.w3.org/2000/svg".into()),
         ],
