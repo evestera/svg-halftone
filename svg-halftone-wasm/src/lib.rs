75412eaf62fcbd7d581f868c75a80469c6b59db3
Erik Vesteraas <erik@vestera.as>
2019-03-17 23:46:57 +0100

    Add wasm-based webpage to project

---
+++ svg-halftone-wasm/src/lib.rs
@@ -0,0 +1,63 @@
+use cfg_if::cfg_if;
+use std::str::FromStr;
+use wasm_bindgen::prelude::*;
+
+use svg_halftone_lib::{create_halftone_svg, image, Grid, Options, Shape};
+
+cfg_if! {
+    // When the `console_error_panic_hook` feature is enabled, we can call the
+    // `set_panic_hook` function to get better error messages if we ever panic.
+    if #[cfg(feature = "console_error_panic_hook")] {
+        use console_error_panic_hook::set_once as set_panic_hook;
+    } else {
+        #[inline]
+        fn set_panic_hook() {}
+    }
+}
+
+cfg_if! {
+    // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
+    // allocator.
+    if #[cfg(feature = "wee_alloc")] {
+        #[global_allocator]
+        static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+    }
+}
+
+#[wasm_bindgen]
+pub fn run(
+    bytes: &[u8],
+    output_width: f64,
+    spacing: f64,
+    shape: &str,
+    grid: &str,
+    invert: bool,
+    cut_paths: bool,
+) -> Option<String> {
+    set_panic_hook();
+
+    let img = match image::load_from_memory(bytes) {
+        Ok(img) => img,
+        Err(_) => return None
+    };
+
+    let (shape, grid) = match (Shape::from_str(shape).ok(), Grid::from_str(grid).ok()) {
+        (Some(shape), Some(grid)) => (shape, grid),
+        (Some(shape), None) => (shape, Grid::from(shape)),
+        (None, Some(grid)) => (Shape::from(grid), grid),
+        (None, None) => (Shape::Circle, Grid::Rect),
+    };
+
+    let data = create_halftone_svg(Options {
+        image: img,
+        output_width,
+        spacing,
+        shape,
+        grid,
+        invert,
+        cut_paths,
+        contrast: None,
+    });
+
+    Some(format!("{}", data))
+}
---
+++ svg-halftone-web/src/index.js
@@ -0,0 +1,57 @@
+import("../../svg-halftone-wasm/pkg").then(module => {
+  const render = () => {
+    const input = document.getElementById('input');
+    const files = input.files;
+    if (files.length == 0) {
+      return;
+    }
+    const file = files[0];
+    document.getElementById('inputname').innerHTML = file.name;
+
+    const outputwidthInput = document.getElementById('outputwidth');
+    const outputwidth = parseFloat(outputwidthInput.value);
+    if (Number.isNaN(outputwidth) || outputwidth === 0.0) {
+      return;
+    }
+
+    const spacingInput = document.getElementById('spacing');
+    const spacing = parseFloat(spacingInput.value);
+    if (Number.isNaN(spacing) || spacing === 0.0) {
+      return;
+    }
+
+    const invert = !document.getElementById('invert').checked;
+    const cutpaths = document.getElementById('cutpaths').checked;
+
+    const reader = new FileReader();
+    reader.onload = () => {
+      const buffer = reader.result;
+      const arr = new Uint8Array(buffer);
+      const result = module.run(
+        arr,
+        outputwidth,
+        spacing,
+        document.getElementById('shape').value,
+        document.getElementById('grid').value,
+        invert,
+        cutpaths
+      );
+      const target = document.getElementById('target');
+      target.innerHTML = result;
+
+      const svgDoc = '<?xml version="1.0" encoding="UTF-8" standalone="no"?>' + result;
+      const encoded = btoa(svgDoc);
+      const downloadLink = document.getElementById('download');
+      downloadLink.href = 'data:application/octet-stream;base64,' + encoded;
+    };
+    reader.readAsArrayBuffer(file);
+  };
+
+  document.getElementById('input').onchange = render;
+  document.getElementById('outputwidth').onchange = render;
+  document.getElementById('spacing').onchange = render;
+  document.getElementById('shape').onchange = render;
+  document.getElementById('grid').onchange = render;
+  document.getElementById('invert').onchange = render;
+  document.getElementById('cutpaths').onchange = render;
+});
